d321afb78d5bf4dd174b0387d169fe4da869b34c
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-16 23:57:15 +0800

    Add component HobbyList

--- src/components/cats/CatPage.js
+++ src/components/cats/CatPage.js
@@ -9,23 +9,38 @@ class CatPage extends React.Component {
         <p>breed: {this.props.cat.breed}</p>
         <p>weight: {this.props.cat.weight}</p>
         <p>temperament: {this.props.cat.temperament}</p>
+        <HobbyList hobbies={this.props.catHobbies} />
       </div>
     );
   }
 }
 
 CatPage.propTypes = {
+  cat: PropTypes.object.isRequired,
   cats: PropTypes.array.isRequired
 };
 
+function collectCatHobbies(hobbies, cat) {
+  let selected = hobbies.map(hobby => {
+    if(cat.hobby_ids.filter(hobbyId => hobbyId == hobby.id).length >0) {
+      return hobby
+    }
+  })
+}
+
 function mapStateToProps(state, ownProps) {
   let cat = {name: '', breed: '', weight: '', temperament:'', hobby_ids: []};
+  let catHobbies = []
   const catId = ownProps.params.id;
-  if(state.cats.length > 0) {
+  if(state.cats.length > 0 && state.hobbies > 0) {
     cat = Object.assign({}, state.cats.find(cat => cat.id == id))
+    if(cat.hobby_ids.length > 0) {
+      hobbies = collectCatHobbies(state.hobbies, cat)
+    }
   }
-  return {cat: cat};
+  return {cat: cat, catHobbies: catHobbies};
 }
+
 
 export default connect(mapStateToProps)(CatPage);
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import {loadCats} from "./actions/catActions";
+import {loadCats} from './actions/catActions';
+import {loadHobbies} from './actions/hobbyActions';
 
 const store = configureStore();
-store.dispatch(loadCats());
 
-render (
+store.dispatch(loadCats());
+store.dispatch(loadHobbies());
+
+render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
   </Provider>,
