cbb22bb130efa0bd37737437041e9553a9cbca2b
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-16 23:41:58 +0800

    Add hobbyApi

--- src/components/cats/CatPage.js
+++ src/components/cats/CatPage.js
@@ -1,6 +1,16 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+
 class CatPage extends React.Component {
   render() {
-
+    return (
+      <div className="col-md-8 col-md-offset-2">
+        <h1>{this.props.cat.name}</h1>
+        <p>breed: {this.props.cat.breed}</p>
+        <p>weight: {this.props.cat.weight}</p>
+        <p>temperament: {this.props.cat.temperament}</p>
+      </div>
+    );
   }
 }
 
